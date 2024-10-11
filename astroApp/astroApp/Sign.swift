3e7ee5d0b91cd23551ebfdff8b358d63faa5fcd3
parinrava <122297950+parinrava@users.noreply.github.com>
2024-10-11 13:31:32 -0700

    sign struct
    Created the sign struct

---
+++ astroApp/astroApp/Sign.swift
@@ -0,0 +1,19 @@
+//
+//  Sign.swift
+//  astroApp
+//
+//  Created by Parin Ravanbakhsh on 2024-10-11.
+//
+
+import Foundation
+struct Sign{
+    var name : String
+    var glyph: String
+    var eligibleDate: String
+    var startMonth: Int
+    var startDay: Int
+    var endMonth: Int
+    var endDay: Int
+    var imagename: String
+    var description: String
+}
