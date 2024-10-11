7d61fac764aa336c816907a38bbee891a34e83b7
parinrava <122297950+parinrava@users.noreply.github.com>
2024-10-11 13:02:14 -0700

    initial commit

---
+++ astroApp/astroApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  astroApp
+//
+//  Created by Parin Ravanbakhsh on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ astroApp/astroApp/astroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  astroAppApp.swift
+//  astroApp
+//
+//  Created by Parin Ravanbakhsh on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct astroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
